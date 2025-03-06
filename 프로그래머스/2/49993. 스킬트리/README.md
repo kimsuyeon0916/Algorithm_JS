361fb4f3285c6fc9f55835271963c2888b1ccb3d
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-03-06 16:06:01 +0900

    [level 2] Title: 스킬트리, Time: 3.89 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#fn1) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 3.62 ms
+메모리: 33.5 MB, 시간: 3.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 06일 15:52:52
+2025년 03월 06일 16:05:59
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,12 +1,12 @@
 function solution(skill, skill_trees) {
     var answer = skill_trees.length;
     
-    for(let i = 0; i<= skill_trees.length; i++) {
+    for(let i = 0; i < skill_trees.length; i++) {
         let idx = -1;
         
-        for(let j = 0; j<= skill_trees[i]?.length;j++){
+        for(let j = 0; j <= skill_trees[i]?.length; j++){
             if(skill.indexOf(skill_trees[i][j]) !== -1){
-                if(skill.indexOf(skill_trees[i][j])>idx && idx+1===skill.indexOf(skill_trees[i][j])){
+                if(skill.indexOf(skill_trees[i][j]) > idx && idx+1 === skill.indexOf(skill_trees[i][j])){
                     idx=skill.indexOf(skill_trees[i][j]);
                     continue;
                 }
