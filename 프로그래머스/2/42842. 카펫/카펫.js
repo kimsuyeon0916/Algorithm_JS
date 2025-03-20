ce49d79342a53b5350988c2b0eaa6e782065a88c
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-03-20 18:25:46 +0900

    [level 2] Title: 카펫, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,34 +1,13 @@
-const 약수배열구하기 = (num) => {
-    let result = []
-    let index = 1;
-    while (index <= num / 2) {
-      if (num % index === 0) result.push(index)
-      index++
-    }
-    result = [...result, num] 
-    return result;
-}
-
 function solution(brown, yellow) {
-    var answer = [];
-    const sum = brown + yellow;
-    
-    const arr = 약수배열구하기(yellow);
-    
-    for(let i=arr.length-1; i>=0; i--){
-        const a = arr[i];
-        const b = yellow / a;
+    const total = brown + yellow;
 
-        if((a+2)*(b+2)===sum){
-            answer.push(a+2);
-            answer.push(b+2);
-            
-            return answer;
+    for (let height = 1; height <= Math.sqrt(yellow); height++) {
+        if (yellow % height === 0) {
+            const width = yellow / height;
+
+            if ((width + 2) * (height + 2) === total) {
+                return [width + 2, height + 2];
+            }
         }
     }
 }
-
-// 2 1 -> 4 * 3 = 10 + 2
-// 1 1 -> 3 * 3 = 8 + 1
-// 8 3 -> 10 * 5 != 24 + 24
-// 6 4 -> 8 * 6 = 24 + 24
