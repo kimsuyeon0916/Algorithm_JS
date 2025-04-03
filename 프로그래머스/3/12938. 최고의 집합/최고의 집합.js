60bba8fdaf22e9a9f7f1c9deeec82f67df168b3b
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-04-03 11:08:55 +0900

    [level 3] Title: 최고의 집합, Time: 0.82 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -0,0 +1,19 @@
+function solution(n, s) {
+    var answer = [-1];
+    let 몫 = Math.floor(s/n);
+    let 나머지 = s%n;
+    answer = 몫>0 ? Array(n).fill(몫) : [-1]
+    for(let i=0;i<answer.length && 몫>0;i++){
+        if(나머지>0){
+            answer[i]+=1;
+            나머지--;
+        }
+    }
+    answer.sort((a,b) => a-b);
+    return answer;
+}
+
+// 처음엔 조합인가? 싶었음. 근데 한 숫자를 다 분배해서 구현하기 어려움...
+// 규칙을 찾아보니 가장 골고루 분배한게 가장 큼
+
+// 3 10 => 4 3 3 몫: 3
